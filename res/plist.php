2d845c93e9f9918f4c6555e7a63bfccb5aecf82e
Jens <jens@northcode.no>
2015-10-20 19:58:49 +0200

    Moved productlist to res/plist.php Fixes #5

---
+++ res/plist.php
@@ -0,0 +1,26 @@
+<?php 
+
+
+$artikel_namen[100] = "Radium (200g Hochradioaktiv)";
+$artikel_preis[100] = 24500.00;
+$artikel_namen[101] = "Berillium (100g)";
+$artikel_preis[101] = 250.00;
+$artikel_namen[102] = "Seifenschalen (nicht an Lager)";
+$artikel_preis[102] = 5.90;
+$artikel_namen[1] = "Seife";
+$artikel_preis[1] = 3.50;
+$artikel_namen[2] = "Milch";
+$artikel_preis[2] = 1.70;
+$artikel_namen[3] = "Socken";
+$artikel_preis[3] = 4.90;
+$artikel_namen[4] = "Spam (Dose)";
+$artikel_preis[4] = 3.50;
+$artikel_namen[5] = "Tee";
+$artikel_preis[5] = 2.65;
+$artikel_namen[6] = "Yoghurt";
+$artikel_preis[6] = 0.65;
+$artikel_namen[7] = "CD-RW";
+$artikel_preis[7] = 5.95;
+
+
+ ?>
