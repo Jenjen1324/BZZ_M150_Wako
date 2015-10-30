7d93b9ab458cfee5b70d7f9fff4b7c451dbb84b2
Jens <jens@northcode.no>
2015-10-30 22:55:25 +0100

    Migratet product list over to MySQL Database Database creation file included Fixes #14

--- html/res/plist.php
+++ html/res/plist.php
@@ -1,26 +1,20 @@
 <?php 
 
+$artikel_namen = array();
+$artikel_preis = array();
 
-$artikel_namen[100] = "Radium (200g Hochradioaktiv)";
-$artikel_preis[100] = 24500.00;
-$artikel_namen[101] = "Berillium (100g)";
-$artikel_preis[101] = 250.00;
-$artikel_namen[102] = "Seifenschalen (nicht an Lager)";
-$artikel_preis[102] = 5.90;
-$artikel_namen[1] = "Seife";
-$artikel_preis[1] = 3.50;
-$artikel_namen[2] = "Milch";
-$artikel_preis[2] = 1.70;
-$artikel_namen[3] = "Socken";
-$artikel_preis[3] = 4.90;
-$artikel_namen[4] = "Spam (Dose)";
-$artikel_preis[4] = 3.50;
-$artikel_namen[5] = "Tee";
-$artikel_preis[5] = 2.65;
-$artikel_namen[6] = "Yoghurt";
-$artikel_preis[6] = 0.65;
-$artikel_namen[7] = "CD-RW";
-$artikel_preis[7] = 5.95;
+$mysql = new mysqli("127.0.0.1", "root", "1234", "m150_wako");
 
+$stmt = $mysql->prepare("SELECT id, name, preis FROM produkte");
+$stmt->execute();
+$stmt->bind_result($id, $name, $preis);
 
+while($stmt->fetch())
+{
+	$artikel_namen[$id] = $name;
+	$artikel_preis[$id] = $preis;
+}
+
+$stmt->close();
+$mysql->close();
  ?>
